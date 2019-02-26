fc2f76c67348004dec011da046ae312a4b8ce16e
SeulGiLee <ghre55@git.co.kr>
2019-02-26 17:48:11 +0900

    최종업데이트

--- val/start.bat
+++ val/start.bat
@@ -1,4 +1,5 @@
 @echo off
-"%JAVA_HOME%\bin\java" -Dfile.encoding=utf-8 -Djava.file.encoding=UTF-8 -jar -Xms1024m -Xmx1024m C:\Users\GIT\Desktop\val\val.jar --basedir C:\Users\GIT\Desktop\val --filetype shp --cidx 2 --layerdefpath C:\Users\GIT/Desktop/sample/digitalmap20_layer.json --valoptpath C:\Users\GIT/Desktop/sample/digitalmap20_option.json --objfilepath C:\Users\GIT/Desktop/sample/digitalmap20.zip --crs EPSG:5186
+"%JAVA_HOME%\bin\java" -Dfile.encoding=UTF-8 -Djava.file.encoding=UTF-8 -jar -Xms256m -Xmx2048m C:\val\val.jar --basedir=C:\val --filetype=shp --cidx=2 --layerdefpath=C:\val\digitalmap20_layer.json --valoptpath=C:\val\digitalmap20_option.json --objfilepath=C:\val\digitalmap20.zip --crs=EPSG:5186
 
+EXIT
 pause>nul
